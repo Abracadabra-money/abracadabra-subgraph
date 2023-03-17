d2b77cb25508dc126ccdc247dd2cb7426a54f088
Alex <tarkovskiy.alexandr@protonmail.com>
2023-03-17 13:34:11 +0200

    update glp

--- src/mappings/magic-glp.ts
+++ src/mappings/magic-glp.ts
@@ -1,13 +1,15 @@
-import { Address } from '@graphprotocol/graph-ts';
+import { Address, dataSource } from '@graphprotocol/graph-ts';
 import { Transfer } from '../../generated/MagicGlp/WETH';
-import { MAGIC_GLP, DEFAULT_DECIMALS } from '../constants';
+import { DEFAULT_DECIMALS } from '../constants';
 import { getOrCreateMagicGlp } from '../helpers/magic-glp/get-or-create-magic-glp';
 import { bigIntToBigDecimal } from '../utils';
+import { getMagicGlpAddress } from '../helpers/get-magic-glp-address';
 
 export function handleLogTransfer(event: Transfer): void {
     const address = event.params.to;
 
-    if (!Address.fromString(MAGIC_GLP).equals(address)) return;
+    const magicGlpAddress = getMagicGlpAddress(dataSource.network());
+    if (!Address.fromString(magicGlpAddress).equals(address)) return;
 
     const magicGlp = getOrCreateMagicGlp();
     const amount = bigIntToBigDecimal(event.params.value, DEFAULT_DECIMALS);
