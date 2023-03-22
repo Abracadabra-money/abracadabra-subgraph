90a02ea7c2f248e9221c10363c00a986d2c15ffd
Alex <tarkovskiy.alexandr@protonmail.com>
2023-03-22 14:00:22 +0200

    prettier

--- src/utils/get-liquidation-price.ts
+++ src/utils/get-liquidation-price.ts
@@ -4,7 +4,7 @@ import { BIGDECIMAL_ZERO } from '../constants';
 import { bigIntToBigDecimal } from './big-int-to-big-decimal';
 
 export function getLiquidationPrice(cauldron: Cauldron, collateral: Collateral, accountState: AccountState): BigDecimal {
-    if(accountState.borrowPart.isZero() || accountState.collateralShare.isZero()) return BIGDECIMAL_ZERO;
+    if (accountState.borrowPart.isZero() || accountState.collateralShare.isZero()) return BIGDECIMAL_ZERO;
     const borrow = bigIntToBigDecimal(accountState.borrowPart);
     const collateralShare = bigIntToBigDecimal(accountState.collateralShare, collateral.decimals);
     const liquidationMultiplier = bigIntToBigDecimal(cauldron.collaterizationRate, 5);
