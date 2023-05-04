ecc27f6628fab6cbdc88f243a919739fa31da40f
Alex <tarkovskiy.alexandr@protonmail.com>
2023-05-04 16:11:23 +0300

    fix yaml

--- src/mappings/level-finance.ts
+++ src/mappings/level-finance.ts
@@ -2,5 +2,5 @@ import { LogHarvest } from '../../generated/LevelFinance/MagicLevelHarvestor';
 import { updateLevelFinanceRewards } from '../helpers/updates';
 
 export function handleLogHarvest(event: LogHarvest): void {
-    updateLevelFinanceRewards(event.params.vault, event.params.amount);
+    updateLevelFinanceRewards(event.params.vault, event.params.amount, event.block);
 }
