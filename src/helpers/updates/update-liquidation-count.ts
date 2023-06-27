ee203649fd3a3ae0b7c038a229272eae4a4a850c
Alex <tarkovskiy.alexandr@protonmail.com>
2023-06-27 15:50:05 +0300

    update updateLiquidationCount

--- src/helpers/updates/update-liquidation-count.ts
+++ src/helpers/updates/update-liquidation-count.ts
@@ -5,20 +5,20 @@ import { getOrCreateUsageCauldronMetricsDailySnapshot } from '../cauldron';
 
 export function updateLiquidationCount(cauldron: Cauldron, account: Account, block: ethereum.Block): void {
     const protocol = getOrCreateProtocol();
-    protocol.liquidationCount += 1;
+    protocol.liquidationCount = protocol.liquidationCount + 1;
     protocol.save();
 
-    cauldron.liquidationCount += 1;
+    cauldron.liquidationCount = cauldron.liquidationCount + 1;
     cauldron.save();
 
-    account.liquidationCount += 1;
+    account.liquidationCount = account.liquidationCount + 1;
     account.save();
 
     const protocolSnapshot = getOrCreateUsageProtocolMetricsDailySnapshot(block);
-    protocolSnapshot.liquidationCount += 1;
+    protocolSnapshot.liquidationCount = protocolSnapshot.liquidationCount + 1;
     protocolSnapshot.save();
 
     const cauldronSnapshot = getOrCreateUsageCauldronMetricsDailySnapshot(cauldron, block);
-    cauldronSnapshot.liquidationCount += 1;
+    cauldronSnapshot.liquidationCount = cauldronSnapshot.liquidationCount + 1;
     cauldronSnapshot.save();
 }
