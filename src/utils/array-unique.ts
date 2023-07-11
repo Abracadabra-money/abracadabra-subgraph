58ee51ad85553fe4656852de4dab54e633b525d1
Alex <tarkovskiy.alexandr@protonmail.com>
2023-07-11 11:55:12 +0300

    lint

--- src/utils/array-unique.ts
+++ src/utils/array-unique.ts
@@ -2,7 +2,7 @@ export function arrayUnique<T>(array: T[]): T[] {
     let unique: T[] = new Array<T>();
     for (let i: i32 = 0; i < array.length; i++) {
         if (!array.includes(array[i])) {
-            unique.push(array[i])
+            unique.push(array[i]);
         }
     }
 
