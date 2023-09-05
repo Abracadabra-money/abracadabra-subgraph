dc4dfeca26fa7bca4511957bd9eba4c204a35746
Alex <tarkovskiy.alexandr@protonmail.com>
2023-09-05 17:31:16 +0300

    update isLiquidate

--- src/utils/is-liquidate.ts
+++ src/utils/is-liquidate.ts
@@ -2,7 +2,7 @@ import { ethereum } from '@graphprotocol/graph-ts';
 
 export function isLiquidate(event: ethereum.Event): boolean {
     const method = event.transaction.input.toHexString().substr(0, 10);
-    return method === '0x912860c5' || method === '0xff6ff84b';
+    return method == '0x912860c5' || method == '0xff6ff84b';
 }
 
 // 0x912860c5 -> liquidate(address[],uint256[],address,address)
